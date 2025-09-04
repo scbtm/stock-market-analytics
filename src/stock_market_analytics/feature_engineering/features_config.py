df2013807a3dd6a5d9a76d655941ff65c78b5f4f
scbtm <128545782+scbtm@users.noreply.github.com>
2025-09-04 19:33:30 +0000

    updated features config

--- src/stock_market_analytics/feature_engineering/features_config.py
+++ src/stock_market_analytics/feature_engineering/features_config.py
@@ -1,6 +1,6 @@
 features_config = {}
 
-features_config["long_window"] = 21
+features_config["long_window"] = 28
 features_config["short_window"] = 14
-features_config["horizon"] = 7
-features_config["past_horizon"] = 252 * 3  # 2 years of lookback window for training
+features_config["horizon"] = 14
+features_config["past_horizon"] = 364 * 3  # 364 * n years of lookback window for training
